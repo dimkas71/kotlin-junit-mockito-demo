26316cf4798652a126dd15680162de990ece547b
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-05-13 17:46:36 +0300

    Adding NullSafety tests and there is an example of using the Tag annotaion

--- src/main/kotlin/ua/compservice/Main.kt
+++ src/main/kotlin/ua/compservice/Main.kt
@@ -1,10 +1,33 @@
 package ua.compservice
 
+val data = (1..100).toMutableList()
+
+enum class Orientation {
+    HORIZONTAL,
+    VERTICAL
+}
+
+data class Layout(var orientation: Orientation)
+
+
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val prettl = Prettl("Another Super cool prettl company", "Prettl super company")
-        println(prettl)
+
+        data.filter { it % 2 == 0 }
+                .forEach { println(it) }
+
+
+        val oldLayout = Layout(Orientation.HORIZONTAL)
+
+        println(oldLayout)
+
+        val newLayout = oldLayout.apply {
+            orientation = Orientation.VERTICAL
+        }
+
+        println(newLayout)
+
     }
 }
---
+++ src/test/kotlin/ua/compservice/NullableSafetyTest.kt
@@ -0,0 +1,33 @@
+package ua.compservice
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+
+@DisplayName("A bunch of tests for better understanding nullable in kotlin")
+@Tag("Nullable")
+class NullableSafetyTest {
+
+    @Test
+    fun `when ask for any property or call any method on the null string then null should be returned`() {
+        var aString: String? = "A string"
+        aString = null
+
+        Assertions.assertAll(
+            Executable { Assertions.assertEquals(null, aString?.length, {"should be null"}) },
+            Executable { Assertions.assertEquals(null, aString?.get(0), {"should be null"})}
+        )
+    }
+
+    @Test
+    fun `when call a method on a nullable string then default value should be returned`() {
+        var str: String? = "something"
+        str = null
+
+        val expected = 'a'
+
+        Assertions.assertEquals(expected, str?.get(0) ?: expected)
+    }
+}

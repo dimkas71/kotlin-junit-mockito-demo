7389ae08b3f9d25c0e1053f673aa8c3bcac90af8
dimkas71 <dimkas71g@gmail.com>
2018-05-14 13:58:06 +0300

    Adding examples of using mockito framework

---
+++ src/test/kotlin/ua/compservice/MockitoWithKotlinTest.kt
@@ -0,0 +1,59 @@
+package ua.compservice
+
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.mockito.Mockito
+
+
+@DisplayName("Examples for using mockito  with kotlin")
+@Tag("Nullable")
+class MockitoWithKotlinTest {
+
+    lateinit var bookService: BookService
+    lateinit var manager: LandBookManager
+
+    @BeforeEach
+    fun setUp() {
+        bookService = Mockito.mock(BookService::class.java)
+
+        manager = LandBookManager(bookService)
+    }
+
+    @Test
+    fun `when book is not available then an exception is thrown`() {
+        Mockito.`when`(bookService.inStock(100)).thenReturn(false);
+        val exception = assertThrows<IllegalStateException>() {
+            manager.checkout(100, 1)
+        }
+        assertEquals("Book is not available", exception.message);
+    }
+
+    @Test
+    fun `when book is available then lend method is called`() {
+        Mockito.`when`(bookService.inStock(100)).thenReturn(true)
+        manager.checkout(100, 1)
+        Mockito.verify(bookService).lend(100, 1)
+    }
+
+
+
+
+}
+
+
+interface BookService {
+    fun inStock(bookId: Int): Boolean
+    fun lend(bookId: Int, memberId: Int)
+}
+
+class LandBookManager(val bookService: BookService) {
+
+    fun checkout(bookId: Int, memberId: Int) {
+        if (bookService.inStock(bookId)) {
+            bookService.lend(bookId, memberId)
+        } else {
+            throw IllegalStateException("Book is not available")
+        }
+    }
+
+}
